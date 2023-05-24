f8094942c66b87064c012e80cf9307d993d4f3ef
Shobayo Samuel <shobayosamuel63@gmail.com>
2023-05-24 06:03:20 -0700

    fix: update models abd views

--- sweepconnect/app/models.py
+++ sweepconnect/app/models.py
@@ -21,11 +21,17 @@ class Cleaner(models.Model):
         return self.user.username
 
 class Booking(models.Model):
+    STATUS_CHOICES = (
+            ('PENDING', 'Pending'),
+            ('PAID', 'Paid'),
+            ('COMPLETED', 'Completed'),
+            ('CANCELLED', 'Cancelled'),
+            )
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     cleaner = models.ForeignKey(Cleaner, on_delete=models.CASCADE)
     start_time = models.DateTimeField()
     end_time = models.DateTimeField()
-    status = models.CharField(max_length=20)
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='PENDING')
     total_cost = models.DecimalField(max_digits=10, decimal_places=2)
 
     def __str__(self):
@@ -39,4 +45,4 @@ class Review(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-                return f"Review {self.id}"
+               return f"Review {self.id}"
--- sweepconnect/app/views.py
+++ sweepconnect/app/views.py
@@ -1,3 +1,47 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+import stripe
+from django.views.decorators.csrf import csrf_exempt
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from .models import Cleaner, Booking
 
-# Create your views here.
+stripe.api_key = settings.STRIPE_SECRET_KEY
+
+def index(request):
+    return HttpResponse("Hello Geeks")login_required
+def home(request):
+    user = request.user
+    cleaners = Cleaner.objects.filter(location=user.location)
+    return render(request, 'home.html', {'cleaners': cleaners})
+
+@login_required
+def book_cleaner(request, cleaner_id):
+    user = request.user
+    try:
+        cleaner = Cleaner.objects.get(id=cleaner_id)
+        booking = Booking.objects.create(user=user, cleaner=cleaner)
+        # Perform any additional logic or actions for the booking
+        return redirect('payment', booking_id=booking.id)
+    except Cleaner.DoesNotExist:
+        return redirect('home')
+
+@login_required
+def payment(request, booking_id):
+    try:
+        booking = Booking.objects.get(id=booking_id)                                    # Perform payment processing logic here
+        if request.method == 'POST':
+            # Process the payment and update the booking status
+            booking.status = 'PAID'
+            booking.save()
+            return redirect('booked', booking_id=booking.id)
+        return render(request, 'payment.html', {'booking': booking})
+    except Booking.DoesNotExist:
+        return redirect('home')
+
+@login_required
+def booked(request, booking_id):
+    try:
+        booking = Booking.objects.get(id=booking_id)
+        return render(request, 'booked.html', {'booking': booking})
+    except Booking.DoesNotExist:
+        return redirect('home')
