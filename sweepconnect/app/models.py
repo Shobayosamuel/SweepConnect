8e1179ea1a6cbc06357fb554a79c5e54bd2ecbc7
Shobayosamuel <102698013+Shobayosamuel@users.noreply.github.com>
2023-05-25 17:27:05 +0100

    Update models.py

--- sweepconnect/app/models.py
+++ sweepconnect/app/models.py
@@ -1,16 +1,19 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.contrib.auth import get_user_model
 
-class User(AbstractUser):
-    name = models.CharField(max_length=255)
-    email = models.EmailField(unique=True)
-    phone_number = models.CharField(max_length=20)
-    class Meta:
-        swappable = 'AUTH_USER_MODEL'
+User = get_user_model()
+
+class Profile(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    id_user = models.IntegerField()
+    bio = models.TextField(blank=True)
+    profileimg = models.ImageField(upload_to='profile_images', default='blank-profile-picture.png')
+    location = models.CharField(max_length=100, blank=True)
 
     def __str__(self):
-        return self.username
-
+        return self.user.username
+    
 class Cleaner(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
     hourly_rate = models.DecimalField(max_digits=6, decimal_places=2)
