8aeb54273bbd191340152a8e90e90eb6528adbfa
Shobayosamuel <102698013+Shobayosamuel@users.noreply.github.com>
2023-05-25 17:22:57 +0100

    Update views.py

--- sweepconnect/app/views.py
+++ sweepconnect/app/views.py
@@ -71,7 +71,7 @@ def stripe_webhook(request):
                 pass
     return JsonResponse({'status': 'success'})
 
-@login_required
+@login_required(login_url='signin')
 def booked(request, booking_id):
     try:
         booking = Booking.objects.get(id=booking_id)
@@ -136,3 +136,31 @@ def signin(request):
 def logout(request):
     auth.logout(request)
     return redirect('signin')
+
+@login_required(login_url='signin')
+def settings(request):
+    user_profile = Profile.objects.get(user=request.user)
+
+    if request.method == 'POST':
+        
+        if request.FILES.get('image') == None:
+            image = user_profile.profileimg
+            bio = request.POST['bio']
+            location = request.POST['location']
+
+            user_profile.profileimg = image
+            user_profile.bio = bio
+            user_profile.location = location
+            user_profile.save()
+        if request.FILES.get('image') != None:
+            image = request.FILES.get('image')
+            bio = request.POST['bio']
+            location = request.POST['location']
+
+            user_profile.profileimg = image
+            user_profile.bio = bio
+            user_profile.location = location
+            user_profile.save()
+        
+        return redirect('settings')
+    return render(request, 'setting.html', {'user_profile': user_profile})
