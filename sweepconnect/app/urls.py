1e321ac06ae7d99651a7533d14ba7b261ebfc745
Shobayosamuel <102698013+Shobayosamuel@users.noreply.github.com>
2023-05-25 17:41:37 +0100

    Update urls.py

--- sweepconnect/app/urls.py
+++ sweepconnect/app/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path('booked/<int:booking_id>/', views.booked, name='booked'),
     path('payment/<int:booking_id>/', views.payment, name='payment'),
     path('webhook/stripe/', views.stripe_webhook, name='stripe_webhook'),
+    path('profile/<str:pk>', views.profile, name='profile'),
     path('signup', views.signup, name='signup'),
     path('signin', views.signin, name='signin'),
     path('logout', views.logout, name='logout'),
